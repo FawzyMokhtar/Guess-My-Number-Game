c8f7812b8fff4b758bd5330cb67194adc141f6d6
FawzyMokhtar <dev.fawzymokhtar@gmail.com>
2022-08-12 04:17:38 +0200

    Use diffrent title's border-width for each platform.

---
+++ components/ui/Title.android.js
@@ -0,0 +1,22 @@
+import { StyleSheet, Text } from 'react-native';
+
+function Title({ children }) {
+  return <Text style={styles.title}>{children}</Text>;
+}
+
+export default Title;
+
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 24,
+    color: 'white',
+    textAlign: 'center',
+    // borderWidth: Platform.select({ ios: 0, android: 2 }),
+    borderWidth: 2,
+    borderColor: 'white',
+    padding: 12,
+    width: 300,
+    maxWidth: '80%',
+  },
+});
--- components/ui/Title.ios.js
+++ components/ui/Title.ios.js
@@ -12,7 +12,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     color: 'white',
     textAlign: 'center',
-    borderWidth: 2,
+    borderWidth: 0,
     borderColor: 'white',
     padding: 12,
     width: 300,
